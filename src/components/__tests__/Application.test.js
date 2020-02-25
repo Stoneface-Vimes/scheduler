969427c414cdbdf7b4ca13fe5806e3ef5e4f0697
Stoneface-Vimes <cliffweir92@gmail.com>
2020-02-25 14:20:17 -0500

    updated the axios mocks to handle put and delete requests. Refactored the test in Application.test.js

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,60 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import {
+  render,
+  waitForElement,
+  fireEvent,
+  prettyDOM,
+  getByText,
+  getAllByTestId,
+  getByPlaceholderText,
+  getByAltText,
+  findByDisplayValue,
+  waitForElementToBeRemoved,
+  findByTestId,
+  queryByText
+} from "@testing-library/react";
 
 import Application from "components/Application";
 
-afterEach(cleanup);
+describe("Application", () => {
 
-xit("renders without crashing", () => {
-  render(<Application />);
-});
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+
+    const { container, debug } = render(<Application />);
+
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    const appointments = getAllByTestId(container, "appointment");
+
+    const appointment = appointments[0]
+
+
+    fireEvent.click(getByAltText(appointment, "Add"))
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"))
+
+    fireEvent.click(getByText(appointment, "Save"))
+
+
+    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+    
+    
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+    );  
+
+
+    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
+  })
+
+})
+
+
+
+
+
