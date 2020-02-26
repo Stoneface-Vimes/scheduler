0320d69a3fa8db61045686b9d47d6610ed9f3ccb
Stoneface-Vimes <cliffweir92@gmail.com>
2020-02-26 13:40:39 -0500

    changed xit's to it's

--- cypress/integration/intergration/appointments.spec.js
+++ cypress/integration/intergration/appointments.spec.js
@@ -9,7 +9,7 @@ describe("Appointments", () => {
     cy.contains("Monday");
   });
 
-  xit("should book an interview", () => {
+  it("should book an interview", () => {
     cy.get("[alt=Add]")
       .first()
       .click();
@@ -23,7 +23,7 @@ describe("Appointments", () => {
     cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
 
-  xit("should edit an interview", () => {
+  it("should edit an interview", () => {
     cy.contains(".appointment__card", "Archie Cohen")
 
     cy.get("[alt=Edit]")
